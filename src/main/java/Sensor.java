1a4c9ac36f941c954c5ec5876c88c410aac458ac
Fereshtaha <90913150+Fereshtaha@users.noreply.github.com>
2022-11-03 14:08:54 +0100

    Updated README + added more files

---
+++ src/main/java/Sensor.java
@@ -0,0 +1,58 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+/**
+ * Represents a resource: a sensor.
+ */
+@Entity
+public class Sensor {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private LocalDateTime dateAndTime;
+    // TODO check if person goes out or inside the building
+    private int totalPeople;
+
+    /**
+     * Empty constructor
+     */
+    public Sensor() {
+    }
+
+    /**
+     * Constructor with parameters
+     * @param id Long
+     */
+    public Sensor(Long id, LocalDateTime dateAndTime, int totalPeople) {
+        this.id = id;
+        this.dateAndTime = dateAndTime;
+        this.totalPeople = totalPeople;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(LocalDateTime dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+    public int getTotalPeople() {
+        return totalPeople;
+    }
+
+    public void setTotalPeople(int totalPeople) {
+        this.totalPeople = totalPeople;
+    }
+}

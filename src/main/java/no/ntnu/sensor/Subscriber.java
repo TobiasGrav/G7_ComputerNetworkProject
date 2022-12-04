6eaa7232ddd5d5439a8bb60877d82e20454b4805
Edvin Astad <Edvin.1996@hotmail.com>
2022-12-04 11:03:14 +0100

    Refactored creator of Subscriber class

--- src/main/java/no/ntnu/sensor/Subscriber.java
+++ src/main/java/no/ntnu/sensor/Subscriber.java
@@ -1,21 +1,39 @@
 package no.ntnu.sensor;
 
 import org.eclipse.paho.client.mqttv3.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Subscriber {
 
     private MqttClient mqttClient;
     private CallBack callBack;
 
-    public Subscriber(String ip, String port) throws MqttException {
-        mqttClient = new MqttClient("tcp://" + ip + ":" + port, MqttClient.generateClientId());
-        callBack = new CallBack();
+    private SensorService sensorService;
+
+
+    public Subscriber(SensorService sensorService) throws MqttException {
+        this.sensorService = sensorService;
+        mqttClient = new MqttClient("tcp://" + "129.241.152.12" + ":" + "1883", MqttClient.generateClientId());
+        callBack = new CallBack(sensorService);
         mqttClient.setCallback(callBack);
         mqttClient.connect();
         System.out.println("--- Connection Established ---");
         mqttClient.subscribe("G7/Test");
         System.out.println("--- Subscribed To Topic ---");
     }
+
+
+//    public Subscriber(String ip, String port) throws MqttException {
+//        mqttClient = new MqttClient("tcp://" + ip + ":" + port, MqttClient.generateClientId());
+//        callBack = new CallBack();
+//        mqttClient.setCallback(callBack);
+//        mqttClient.connect();
+//        System.out.println("--- Connection Established ---");
+//        mqttClient.subscribe("G7/Test");
+//        System.out.println("--- Subscribed To Topic ---");
+//    }
 
     public void close() throws MqttException {
         mqttClient.disconnect();

93262a7cc682e1ac49fd96c04660f5dc00d586fb
edvin <edvina@stud.ntnu.no>
2022-12-05 11:28:28 +0100

    Comments added to Publisher class.

--- src/main/java/no/ntnu/sensor/Publisher.java
+++ src/main/java/no/ntnu/sensor/Publisher.java
@@ -4,15 +4,34 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+/**
+ * Responsible for uploading data to server.
+ *
+ * <b> Uploads data as byte arrays utilizing the Mqtt interface.</b>
+ */
 public class Publisher {
 
     MqttClient mqttClient;
 
+    /**
+     * Responsible for creating a new instance of the Publisher class.
+     *
+     * @param ip IP address of server which is to receive the data.
+     * @param port Port of the server which is to receive the data.
+     * @throws MqttException
+     */
     public Publisher(String ip, String port) throws MqttException {
         mqttClient = new MqttClient("tcp://" + ip + ":" + port, MqttClient.generateClientId());
         mqttClient.connect();
     }
 
+    /**
+     * Responsible for turning a formatted string in to a MqttMessage object.
+     *
+     * @param message String which is to be turned in to a MqttMessage object.
+     *                String must be correctly formatted.
+     * @return Provided data as string as a MqttMessage.
+     */
     public MqttMessage createMessage(String message) {
         MqttMessage mqttMessage = new MqttMessage();
         byte[] byteArray = message.getBytes();
@@ -20,10 +39,20 @@ public class Publisher {
         return mqttMessage;
     }
 
+    /**
+     * Responsible for uploading data to the server.
+     * @param message message which is to be uploaded as String.
+     * @throws MqttException
+     */
     public void publishMessage(String message) throws MqttException {
         mqttClient.publish("G7/Test", createMessage(message));
     }
 
+    /**
+     * Closes connection to server.
+     *
+     * @throws MqttException
+     */
     public void close() throws MqttException {
         mqttClient.disconnect();
         mqttClient.close();
